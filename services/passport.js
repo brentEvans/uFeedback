378d146f47cc0abd0f3efd1f693597ffa55ce092
Brent Evans <brentevans336@gmail.com>
2019-06-17 10:32:29 -0400

    first commit

--- services/passport.js
+++ services/passport.js
@@ -21,27 +21,15 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback',
         proxy: true
-    }, (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleId: profile.id }).then((existingUser) => {
-            if (existingUser) {
-                // we already have a record with the given profile ID
-                // first argument to done() is err, second argument is the user
-                console.log("*"*30);
-                console.log('access token', accessToken);
-                console.log("*"*30);
-                console.log('refresh token', refreshToken);
-                console.log("*"*30);
-                console.log('profile', profile);
+    }, async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({ googleId: profile.id })
 
+        if (existingUser) {
+            return done(null, existingUser);
+        } 
 
-                done(null, existingUser);
-            } else {
-                // we don't have a user record with this ID, make a new record
-                new User({ googleId: profile.id })
-                    .save()
-                    .then(user => done(null, user));
-            }
-        });
+        const user = await new User({ googleId: profile.id }).save()
+        done(null, user);
     })
 );
 
