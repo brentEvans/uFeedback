3cb9ba6e252b876ca2b2ba952c7bc218b1a3eccd
Brent Evans <brentevans336@gmail.com>
2019-06-22 14:37:11 -0400

    finished email feature

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,8 @@ import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
+import axios from 'axios'; // testing email functionality without front-end
+window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
